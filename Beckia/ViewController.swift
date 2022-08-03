2ccdc48261698dd7f241469a9600648115b0d38f
Abril Xu <abrilxu@gmail.com>
2022-08-03 10:44:33 -0400

    added a background

--- Beckia/ViewController.swift
+++ Beckia/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var backgroundGradient: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+   
+    
+         }
 
-
+   
+         
+    
+    
+    
+    
 }
-
